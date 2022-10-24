47865416adffd25a8177a3756cd5f6a9fe1fd32d
ztakis <a-zorbas@otenet.gr>
2022-10-24 14:30:49 +0300

    Gt7jlh3L9

--- python/examples/headerbar2.py
+++ python/examples/headerbar2.py
@@ -10,7 +10,7 @@ class HeaderBarWindow(Gtk.Window):
     def __init__(self):
         super().__init__(title="HeaderBar Demo")
         self.set_border_width(10)
-        self.set_default_size(400, 200)
+        self.set_default_size(800, 480)
 
         hb = Gtk.HeaderBar()
         hb.set_show_close_button(True)
@@ -18,7 +18,8 @@ class HeaderBarWindow(Gtk.Window):
         self.set_titlebar(hb)
 
         button = Gtk.Button()
-        icon = Gio.ThemedIcon(name="mail-send-receive-symbolic")
+        # icon = Gio.ThemedIcon(name="mail-send-receive-symbolic")
+        icon = Gio.ThemedIcon(name="preferences-system-symbolic")
         image = Gtk.Image.new_from_gicon(icon, Gtk.IconSize.BUTTON)
         button.add(image)
         hb.pack_end(button)
@@ -26,12 +27,21 @@ class HeaderBarWindow(Gtk.Window):
         box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
         Gtk.StyleContext.add_class(box.get_style_context(), "linked")
 
-        button = Gtk.Button()
-        button.add(Gtk.Arrow(arrow_type=Gtk.ArrowType.LEFT, shadow_type=Gtk.ShadowType.NONE))
-        box.add(button)
+        home_button = Gtk.Button.new_from_icon_name("go-home", Gtk.IconSize.MENU)
+        box.add(home_button)
 
-        button = Gtk.Button.new_from_icon_name("pan-end-symbolic", Gtk.IconSize.MENU)
-        box.add(button)
+        left_button = Gtk.Button()
+        left_button.add(Gtk.Arrow(arrow_type=Gtk.ArrowType.LEFT, shadow_type=Gtk.ShadowType.NONE))
+        left_button.set_sensitive(False)
+        box.add(left_button)
+
+        right_button = Gtk.Button()
+        right_button.add(Gtk.Arrow(arrow_type=Gtk.ArrowType.RIGHT, shadow_type=Gtk.ShadowType.NONE))
+        right_button.set_sensitive(True)
+        box.add(right_button)
+
+        # button = Gtk.Button.new_from_icon_name("pan-end-symbolic", Gtk.IconSize.MENU)
+        # box.add(button)
 
         hb.pack_start(box)
 
