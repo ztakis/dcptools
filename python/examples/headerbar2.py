bcc6b501010017e2ed599d57d36f3d61d005397b
ztakis <a-zorbas@otenet.gr>
2022-10-24 15:44:38 +0300

    HU97tgdl4kh

--- python/examples/headerbar2.py
+++ python/examples/headerbar2.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python3
 
+import sys
 import gi
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, Gio
 
+# This would typically be its own file
+MENU_XML = """
+<?xml version="1.0" encoding="UTF-8"?>
+<interface>
+  <menu id="sys-menu">
+    <section>
+        <item>
+            <attribute name="label">About</attribute>
+            <attribute name="action">app.about</attribute>
+        </item>
+        <item>
+            <attribute name="label">Quit</attribute>
+            <attribute name="action">app.quit</attribute>
+        </item>
+    </section>
+  </menu>
+</interface>
+"""
 
 class HeaderBarWindow(Gtk.Window):
     def __init__(self):
@@ -17,7 +36,11 @@ class HeaderBarWindow(Gtk.Window):
         hb.props.title = "HeaderBar example"
         self.set_titlebar(hb)
 
-        button = Gtk.Button()
+        builder = Gtk.Builder.new_from_string(MENU_XML, -1)
+        menu = builder.get_object("sys-menu")
+
+        button = Gtk.MenuButton(menu_model=menu)
+        # button = Gtk.Button()
         # icon = Gio.ThemedIcon(name="mail-send-receive-symbolic")
         icon = Gio.ThemedIcon(name="preferences-system-symbolic")
         image = Gtk.Image.new_from_gicon(icon, Gtk.IconSize.BUTTON)
@@ -47,8 +70,43 @@ class HeaderBarWindow(Gtk.Window):
 
         self.add(Gtk.TextView())
 
+# class Application(Gtk.Application):
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, application_id="org.example.myapp", **kwargs)
+#         self.window = None
+
+#     def do_startup(self):
+#         Gtk.Application.do_startup(self)
+
+#         action = Gio.SimpleAction(name="about")
+#         action.connect("activate", self.on_about)
+#         self.add_action(action)
+
+#         action = Gio.SimpleAction(name="quit")
+#         action.connect("activate", self.on_quit)
+#         self.add_action(action)
+
+#     def do_activate(self):
+#         # We only allow a single window and raise any existing ones
+#         if not self.window:
+#             # Windows are associated with the application
+#             # when the last one is closed the application shuts down
+#             self.window = win(application=self, title="HeaderBar Demo")
+
+#         self.window.present()
+
+#     def on_about(self, action, param):
+#         about_dialog = Gtk.AboutDialog(transient_for=self.window, modal=True)
+#         about_dialog.present()
+
+#     def on_quit(self, action, param):
+#         self.quit()
 
 win = HeaderBarWindow()
 win.connect("destroy", Gtk.main_quit)
 win.show_all()
 Gtk.main()
+
+# if __name__ == "__main__":
+#     app = Application()
+#     app.run(sys.argv)
